8a5771ed14dea4c03703a88643da119f432a8917
inakicarcereny <inakicarce@hotmail.com.ar>
2025-03-25 16:03:49 -0300

    Add example usage of string and char with console output in text.cs for C# programming introduction

---
+++ text.cs
@@ -0,0 +1,12 @@
+string name = "Iñaki";
+char letter = 'a';
+
+Console.WriteLine(name);
+Console.WriteLine(letter);
+
+Console.Write("Enter your name: ");
+Console.WriteLine(name);
+Console.WriteLine();
+Console.WriteLine(letter);
+Console.WriteLine();
+Console.WriteLine("Hello how are you?");
