126e1a665975b7bcffb350d416f253b92a23aa2a
inakicarcereny <inakicarce@hotmail.com.ar>
2025-03-25 15:52:36 -0300

    Add variable declarations and console output examples to basics.cs for intro to C# programming

--- basics.cs
+++ basics.cs
@@ -1,2 +1,41 @@
 Console.WriteLine("Hello World!");
-Console.ReadLine();
+
+// int x = 10;
+// int y = 20;
+// int z = 30
+
+int x = 10,
+    y = 20,
+    z = 30;
+
+int age = 23;
+
+long bigNumber = 1234567890123456789;
+
+double negativeValue = -123.456;
+
+float floatValue = 123.456f;
+
+decimal decimalValue = 123.456m;
+
+Console.WriteLine(age);
+Console.WriteLine(int.MaxValue);
+Console.WriteLine(int.MinValue);
+
+Console.WriteLine(bigNumber);
+Console.WriteLine(long.MaxValue);
+Console.WriteLine(long.MinValue);
+
+Console.WriteLine(negativeValue);
+Console.WriteLine(double.MaxValue);
+Console.WriteLine(double.MinValue);
+
+Console.WriteLine(floatValue);
+Console.WriteLine(float.MaxValue);
+Console.WriteLine(float.MinValue);
+
+Console.WriteLine(decimalValue);
+Console.WriteLine(decimal.MaxValue);
+Console.WriteLine(decimal.MinValue);
+
+// Console.ReadLine();
