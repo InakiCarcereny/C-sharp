cc787a315ce339c6fe8a2632d569144e88951adc
inakicarcereny <inakicarce@hotmail.com.ar>
2025-03-25 16:30:19 -0300

    Add arithmetic and string manipulation examples with console output in operators.cs for C# programming introduction

---
+++ operators.cs
@@ -0,0 +1,18 @@
+int age = 23;
+
+age++;
+age+=1;
+age = age + 1;
+Console.WriteLine(age);
+
+age--;
+age-=1;
+age = age - 1;
+Console.WriteLine(age);
+
+age *= 2;
+age /= 2; // se basa en el tipo de dato, si fuese por ejemplo un double, me devolveria 2.3
+
+string name = "Iñaki";
+name += " is a developer";
+Console.WriteLine(name);
