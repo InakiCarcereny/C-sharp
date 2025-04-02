3f5d99a4cd3e08be0bdccf971ad61430f2e4dd25
inakicarcereny <inakicarce@hotmail.com.ar>
2025-04-02 12:52:24 -0300

    .

---
+++ parse.cs
@@ -0,0 +1,9 @@
+int edad;
+texto_edad = "40"
+edad = int.Parse(texto_edad)
+edad = Convert.ToInt32(texto_edad)
+
+double litros = 14.95
+int latas_1lt;
+latas_1lt = (int)litros
+latas_1lt = Convert.ToInt32(litros)
