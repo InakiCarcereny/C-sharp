62cd74b1743ac9e019fe1e000ad9092a44bc0ece
inakicarcereny <inakicarce@hotmail.com.ar>
2025-04-12 15:15:16 -0300

    .

---
+++ basics/format.cs
@@ -0,0 +1,13 @@
+namespace sum_two_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double price = 10 / 3;
+
+            Console.WriteLine($"price: {price:F2}");
+            Console.WriteLine(price.ToString());
+        }
+    }
+}
