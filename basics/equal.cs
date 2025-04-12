37c8f70779a047902c660a2e4dd750f6c49abc58
inakicarcereny <inakicarce@hotmail.com.ar>
2025-04-12 15:15:11 -0300

    .

---
+++ basics/equal.cs
@@ -0,0 +1,20 @@
+namespace sum_two_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello";
+            string other_message = "Hello";
+
+            if (message.Equals(other_message))
+            {
+                Console.WriteLine("The messages are equal");
+            }
+            else
+            {
+                Console.WriteLine("The messages are not equal");
+            }
+        }
+    }
+}
