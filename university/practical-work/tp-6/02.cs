8dc32492fc7988217c567539bee154fd9c955302
inakicarcereny <inakicarce@hotmail.com.ar>
2025-05-20 14:31:57 -0300

    .

---
+++ university/practical-work/tp-6/02.cs
@@ -0,0 +1,9 @@
+a. static void Ej1(char a, char b, out string resultado) { } //Parametros por valor = 2, Parametros por referencia = 1
+
+b. static void Ej2(out char a, out char b, out string resultado) { } //Parametros por referencia = 3
+
+c. static double F1(int num1, double num2, bool realizar) { } //Parametros por valor = 3
+
+d. static char LeeLetra(string texto, int pos) { } //Parametros por valor = 2
+
+e. static int LeeNumero(string texto, out bool error) { } //Parametros por valor = 1, Parametros por referencia = 1
